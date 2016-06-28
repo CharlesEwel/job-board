36f11fd48a09510788897030c92018fefbd0a5c9
Carl Egbert and Charles Ewel <none@none>
2016-06-28 10:53:16 -0700

    create initial files and directories

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+using Nancy;
+using JobBoard.Objects;
+using System.Collections.Generic;
+
+namespace JobBoard
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+
+
+
+    }
+  }
+}
